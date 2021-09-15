b221de8c03a287549d82b428ce0f37e9a26d13fe
YeungWanLum <295386487@qq.com>
2021-09-15 16:39:39 +0800

    update: 更新VuePress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,34 @@ module.exports = {
   title: '黑马头条',
   description: '黑马头条文档',
   themeConfig: {
-    logo: 'https://vuejs.org/images/logo.png',
+    logo: 'https://vuejs.org/images/logo.svg',
+    searchMaxSuggestions: 10,
+    markdown: {
+      lineNumbers: true
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/VueProjectCourse/HeiMaTouTiao' },
+    ],
+    sidebar: [
+      {
+        title: '1.初始化项目',   // 必要的
+        path: '/initial-project/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/'
+        ]
+      },
+      {
+        title: '2.登录功能',
+        path: '/login/',
+        children: [ /* ... */],
+        initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+      }
+    ]
   },
+  plugins: ['@vuepress/back-to-top']
 }
 
---
+++ docs/initial-project/README.md
@@ -0,0 +1,17 @@
+# 1.初始化项目
+
+## 1.1 创建并梳理项目的结构
+
+## 1.2 了解ESLint常见的语法规则
+
+```bash
+ rules: {
+    // 默认情况下，ESLint 规定：方法的形参之前必须保留一个空格
+    // 'space-before-function-paren': ['error', 'always']
+
+    // 如果指定了规则的值为 never，则不允许在方法的形参之前保留任何空格
+    'space-before-function-paren': ['error', 'never']
+  }
+```
+
+## 1.3 配置vant组件库
