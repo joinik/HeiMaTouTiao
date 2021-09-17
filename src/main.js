6a84b40fc6f039fa66fc3c1ae221850ecfa104e0
YeungWanLum <295386487@qq.com>
2021-09-17 08:33:17 +0800

    初始化并梳理项目结构

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// #region 导入并注册所有的Vant组件
+// 一次性、完整导入并注册所有vant组件的优缺点:
+// 优点: 所有vant组件都进行了全局的注册。在每个组件中，不再需要按需引入并注册组件了
+// 缺点: 项目中没有用到的组件也会被打包进来，导致打包体积过大的问题(此问题在项目发布时，可通过cdn加速解决)
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// #endregion
+
+// #region 配置 amfe-flexible
+import 'amfe-flexible'
+
 Vue.config.productionTip = false
+Vue.use(Vant)
+// #endregion
 
 new Vue({
   router,
---
+++ src/utils/request.js
@@ -0,0 +1,10 @@
+// 定义网络请求模块
+import axios from 'axios'
+
+// 调用axios.create()方法，创建 axios 的实例对象
+const instance = axios.create({
+  //  请求根路径
+  baseURL: 'http://www.liulongbing.top:8080'
+})
+
+export default instance
