d9c1974666bd13cd6d0eb645a06ba0c6838af591
YeungWanLum <295386487@qq.com>
2021-10-21 14:12:24 +0800

    update: 更新 token续签 代码和文档

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { Toast } from 'vant'
 import store from '@/store'
 import router from '../router'
+import { exchangeTokenAPI } from '../API/userAPI'
 // 调用axios.create()方法，创建 axios 的实例对象
 const instance = axios.create({
   //  请求根路径
@@ -33,16 +34,32 @@ instance.interceptors.request.use(config => {
 instance.interceptors.response.use(response => {
   // 隐藏 loading 效果
   Toast.clear()
-  console.log('响应:', response)
   return response
-}, error => {
-  // console.dir(error.response)
-  if (error.response.data.message === '用户未认证') {
-    // 隐藏 loading 效果
-    Toast.clear()
-    router.push('/login')
+}, async error => {
+  // 在请求失败的时候，关闭 loading 提示效果
+  Toast.clear()
+  // 1.从 vuex 中获取 tokenInfo 对象， 格式为：{token, refresh_token}
+  const tokenInfo = store.state.tokenInfo.tokenInfo
+  // 2. 判断是否为 token 过期
+  if (error.response && error.response.status === 401 || tokenInfo.refresh_token) {
+
+    try {
+      // 3.1 TODO: 发起请求，根据 refresh_token 重新请求一个有效的新 token
+      const { data: res } = await exchangeTokenAPI(tokenInfo.refresh_token)
+      // 3.2 TODO: 更新 Store 中的 Token
+      store.commit('updateTokenInfo', { token: res.data.token, refresh_token: tokenInfo.refresh_token })
+        // 3.3 重新调用刚才“请求未遂”的接口
+        // 3.3.1 如果在响应拦截器中 return 一个新的 Promise 异步请求，则会把这次请求的结果，当作上次请求的返回值
+      return instance(error.config)
+    } catch {
+      // token 和 refresh_token 都失效了
+
+      // 4.1 清空 vuex 和 localStorage
+      store.commit('cleanState')
+      // 4.2 强制跳转到登录页
+      router.replace('/login?pre=' + router.currentRoute.fullPath)
+    }
   }
   return Promise.reject(error)
-}
-)
+})
 export default instance
